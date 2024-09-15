70c3e7cde083c70e1243bea044d55b0a413e5655
Minh Nguyen <minh20051202@gmail.com>
2024-09-15 21:55:53 +0700

    update

---
+++ aiken-workspace/off-chain/instantiate-validators.ts
@@ -0,0 +1,22 @@
+import {
+  getCredential,
+  getPublicKeyHash,
+  parseValidatorAndApplyParameters,
+} from "./utils/lucid/utils.js";
+import blueprint from "../plutus.json" assert { type: "json" };
+
+const issuerPublicKeyHash = getPublicKeyHash(
+  await getCredential("issuer.addr")
+);
+
+const storeValidator = parseValidatorAndApplyParameters(
+  blueprint.validators,
+  [issuerPublicKeyHash],
+  "reference_store.store"
+);
+
+const _mintValidator = parseValidatorAndApplyParameters(
+  blueprint.validators,
+  [issuerPublicKeyHash, storeValidator.policyId],
+  "mint.mint"
+);
---
+++ aiken-workspace/off-chain/remove-nft.ts
@@ -0,0 +1,52 @@
+import { Constr, Data } from "lucid";
+import { createLucidInstance, getCredential } from "./utils/lucid/utils.js";
+import { APPLIED_VALIDATOR_PATH } from "./common/constants.js";
+import { AppliedValidator, MetaDatum } from "./common/types.js";
+
+const lucid = await createLucidInstance();
+
+const storeValidator: AppliedValidator = JSON.parse(
+  await Deno.readTextFile(APPLIED_VALIDATOR_PATH + "reference_store.store.json")
+);
+const scriptUtxos = await lucid.utxosAt(storeValidator.lockAddress);
+
+lucid.selectWalletFromPrivateKey(await getCredential("issuer.sk"));
+
+const userAddr = await getCredential("user.addr");
+const rdmr = Data.to(new Constr(1, []));
+// const utxo = scriptUtxos[0];
+
+const tx = lucid.newTx().collectFrom(scriptUtxos, rdmr);
+
+scriptUtxos.forEach((utxo) => {
+  if (utxo.datum) {
+    // Deserialze to MetaDatum to confirm datum type. UTxOs with malformed or
+    // different datum types cannot be removed.
+    try {
+      const _datum = Data.from<MetaDatum>(utxo.datum, MetaDatum);
+      tx.payToAddressWithData(userAddr, { inline: utxo.datum }, utxo.assets);
+    } catch (e) {
+      console.log(
+        "Cannot cast the datum of utxo to object of type MetaDatum. " +
+          e.message
+      );
+      console.log(utxo);
+    }
+  } else console.log("UTxO without inline datum found: " + utxo);
+});
+
+const completedTx = await tx
+  .addSigner(await lucid.wallet.address())
+  .attachSpendingValidator(storeValidator.validator)
+  .complete();
+
+const signedTx = await completedTx.sign().complete();
+const txHash = await signedTx.submit();
+await lucid.awaitTx(txHash);
+
+console.log(
+  `Successfully removed all assets from
+storeAddress: ${storeValidator.lockAddress},
+to userAddress: ${userAddr},
+txHash: ${txHash}`
+);
